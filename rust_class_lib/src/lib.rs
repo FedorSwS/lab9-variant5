1429b20834fbbb033a1f035320d15385a656dfb6
Evstigneev Fedor <feda12348@mail.ru>
2026-03-21 22:17:09 +0300

    feat(rust): implement DataProcessor class with PyO3 bindings

--- rust_class_lib/src/lib.rs
+++ rust_class_lib/src/lib.rs
@@ -1,6 +1,47 @@
 use pyo3::prelude::*;
 
+#[pyclass]
+struct DataProcessor {
+    name: String,
+    processed_count: u32,
+}
+
+#[pymethods]
+impl DataProcessor {
+    #[new]
+    fn new(name: String) -> Self {
+        DataProcessor {
+            name,
+            processed_count: 0,
+        }
+    }
+    
+    fn process(&mut self, data: String) -> String {
+        self.processed_count += 1;
+        format!("[{}] Processed: {}", self.name, data)
+    }
+    
+    fn get_count(&self) -> u32 {
+        self.processed_count
+    }
+    
+    fn reset(&mut self) {
+        self.processed_count = 0;
+    }
+    
+    fn __repr__(&self) -> String {
+        format!("DataProcessor(name='{}', count={})", self.name, self.processed_count)
+    }
+}
+
+#[pyfunction]
+fn fast_compute(numbers: Vec<i64>) -> i64 {
+    numbers.iter().map(|x| x * x).sum()
+}
+
 #[pymodule]
-fn rust_class_lib(_py: Python<'_>, _: &PyModule) -> PyResult<()> {
+fn rust_class_lib(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_class::<DataProcessor>()?;
+    m.add_function(wrap_pyfunction!(fast_compute, m)?)?;
     Ok(())
 }
